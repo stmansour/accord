b9f0c37d20ea5908d07f03e14da95d10451e4cba
Steve Mansour <sman@stevemansour.com>
2015-08-10 15:15:59 -0700

    add windows native unix utils to downloads

--- ci/jenkins/setup-win-jenkins.bat
+++ ci/jenkins/setup-win-jenkins.bat
@@ -29,10 +29,26 @@ REM  -- a bug in cygwin shells causes it to truncate lines after about 153 chars
 REM  -- So, the wget or curl commands end up truncating and failing.
 REM  -- We'll use good ole wget64 to pull down what we need...
 REM
+
 pushd c:\cygwin\home\Administrator
 C:\Users\Administrator\Downloads\wget64.exe -O unzip.exe --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/utils/unzip.exe
 C:\Users\Administrator\Downloads\wget64.exe -O jdk-8u51-windows-x64.exe --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/java/jdk-8u51-windows-x64.exe
 C:\Users\Administrator\Downloads\wget64.exe -O jenkins-1.624.zip --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/utils/jenkins-1.624.zip
+
+REM Here's a note straight from the Jenkins Installation guide:
+REM    Since Jenkins was written to work on unix-like platforms, some parts
+REM    assume the presence of unix-utilities. It is advised to install these as
+REM    well on Windows. Install UnxUtils (this includes a shell that seems to work
+REM    with forward and backwards slashes and does globbing correctly)(UnxUtils
+REM    does not download), put it in the Windows PATH , and copy sh.exe to
+REM    C:\bin\sh.exe (or whichever drive you use) to make shebang lines work.
+REM    This should get you going.
+REM
+REM OK, so based on this we'll download and install UnxUtils.  These look like
+REM a subset of the Cygwin code, but they run as native windows apps using MSCRT.DLL
+REM rather than Cygwin's emulation layer.
+C:\Users\Administrator\Downloads\wget64.exe -O UnxUpdates.zip --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/utils/UnxUpdates.zip
+
 popd
 
 REM
