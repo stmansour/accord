9d5f2cbb2f231b1dc25f728f08d5034361d2e70c
Steve Mansour <sman@stevemansour.com>
2015-08-09 20:46:36 -0700

    force Administrator unix home dir to be created before executing bash script

--- ci/jenkins/setup-win-jenkins.bat
+++ ci/jenkins/setup-win-jenkins.bat
@@ -1,6 +1,27 @@
+REM
+REM  -- Windows Jenkins Setup script
+REM
+
+REM
+REM  -- Download some of the basic stuff we need to get started.
+REM
 wget64 -O cygwin-setup.exe --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/utils/cygwin-setup.exe
 wget64 -O getcygwin.bat --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/utils/getcygwin.bat
 wget64 -O setup-win-jenkins --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/utils/setup-win-jenkins
 call getcygwin.bat
+
+REM
+REM  -- Force Administrator home directory to be created
+REM
+c:\cygwin\bin\bash -l -c pwd
+
+REM
+REM  -- copy the script we need to execut in the bash shell to 
+REM  -- Adminstrator's home directory
+REM
 copy setup-win-jenkins c:\cygwin\home\Administrator
+
+REM
+REM  -- Now we let the bash shell script take over...
+REM
 c:\cygwin\bin\bash -l setup-win-jenkins
