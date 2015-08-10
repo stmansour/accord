725f4b79d6412448876a61d2ea4084af8903e831
Steve Mansour <sman@stevemansour.com>
2015-08-09 22:14:22 -0700

    fix for a bug in cygwin terminals

--- ci/jenkins/setup-win-jenkins.bat
+++ ci/jenkins/setup-win-jenkins.bat
@@ -25,6 +25,17 @@ REM
 copy setup-win-jenkins c:\cygwin\home\Administrator
 
 REM
+REM  -- a bug in cygwin shells causes it to truncate lines after about 153 chars
+REM  -- So, the wget or curl commands end up truncating and failing.
+REM  -- We'll use good ole wget64 to pull down what we need...
+REM
+pushd c:\cygwin\home\Administrator
+C:\Users\Administrator\Downloads\wget64.exe -O unzip.exe --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/utils/unzip.exe
+C:\Users\Administrator\Downloads\wget64.exe -O jdk-8u51-windows-x64.exe --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/java/jdk-8u51-windows-x64.exe
+C:\Users\Administrator\Downloads\wget64.exe -O jenkins-1.624.zip --user accord --password AP4GxDHU2f6EriLqry781wG6fy http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory/ext-tools/utils/jenkins-1.624.zip
+popd
+
+REM
 REM  -- Now we let the bash shell script take over...
 REM
 c:\cygwin\bin\bash -l setup-win-jenkins
