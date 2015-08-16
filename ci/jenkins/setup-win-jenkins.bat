1aa210ae063c1e13014655bc06d783a70f697fe6
Steve Mansour <sman@stevemansour.com>
2015-08-16 10:12:05 -0700

    could not figure out why setx command was making the system unstable. commented out lines that seemed to trigger it. will figure out later.

--- ci/jenkins/setup-win-jenkins.bat
+++ ci/jenkins/setup-win-jenkins.bat
@@ -11,37 +11,36 @@ SET USR=accord
 SET PASS=AP4GxDHU2f6EriLqry781wG6fy
 SET ART=http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory
 SET INSTALLHOME=C:\Users\Administrator\Downloads
-SET ACCORD_ROOT=C:\Program Files
-SET ACCORD_DNAME=Accord
-SET ACCORD_HOME=%ACCORD_ROOT%\%ACCORD_DNAME%
+SET ACCORD_HOME=C:\Accord
 SET WGET=wget64.exe
-SET UNZIP=unzip.exe
-SET UNZIPCMD=7z e
+SET STX=C:\Windows\System32\SETX.exe
 
 ECHO Creating c:\Windows\Accord with unix tools
-PUSHD %ACCORD_ROOT%
-    MKDIR %ACCORD_DNAME%
-    MKDIR %ACCORD_DNAME%\bin
-    SETX /M Path "%ACCORD_HOME%\bin;%PATH%"
-    SET PATH=%ACCORD_HOME%\bin;%PATH%
-    ECHO C:\Program Files\Accord\bin added to the path
-POPD
-COPY wget64.exe "%ACCORD_HOME%\bin"
+MKDIR %ACCORD_HOME%
+MKDIR %ACCORD_HOME%\bin
+COPY wget64.exe %ACCORD_HOME%\bin
 
-REM  -- It does not make sense to perform these at this time
-REM  -- but this bat script starts failing setx commands at 
-REM  -- some point. Haven't figured out what's causing it.
-REM  -- The workaround is to do this stuff up front before
-REM  -- we've actually downloaded and installed these files
-SETX /M JAVA_HOME "C:\Program Files\Java\jdk1.8.0_51"
+REM  -- after a few 'setx /m Path' commands, the system
+REM  -- becomes unstable. Haven't figured this out yet.
+REM  -- for now, minimize the setx calls
+
 SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_51
-SETX /M Path "C:\Program Files\Java\jdk1.8.0_51\bin;%PATH%"
-SET PATH="C:\Program Files\Java\jdk1.8.0_51\bin;%PATH%"
-SETX /M GRADLE_HOME "%ACCORD_HOME%\gradle-2.6"
-SET GRADLE_HOME=%ACCORD_HOME%\gradle-2.6
-SETX /M Path "%GRADLE_HOME%\bin;%PATH%"
-SET PATH=%GRADLE_HOME%\bin;%PATH%
+SET GRADLE_HOME=%ACCORD_HOME%\gradle
 
+echo Java_home
+%STX% /M JAVA_HOME "C:\Program Files\Java\jdk1.8.0_51"
+echo JAVA
+%STX% /M Path "%JAVA_HOME%\bin;%PATH%"
+SET PATH="%JAVA_HOME%\bin;%PATH%"
+echo Gradle_home
+%STX% /M GRADLE_HOME "%ACCORD_HOME%\gradle"
+echo GRADLE
+REM %STX% /M Path "%GRADLE_HOME%\bin;%PATH%"
+REM %STX% /M Path "C:\Accord\gradle\bin;%PATH%"
+SET PATH="%GRADLE_HOME%\bin;%PATH%"
+echo ACCORD
+REM %STX% /M Path "%ACCORD_HOME%\bin;%PATH%"
+SET PATH="%ACCORD_HOME%\bin;%PATH%"
 
 REM
 REM  -- Download the artifacts we need
@@ -54,11 +53,12 @@ CALL :SUB_WGET ext-tools/utils gradle-2.6.tar
 CALL :SUB_WGET ext-tools/utils jenkins-1.624.zip
 CALL :SUB_WGET ext-tools/utils 7z.tar
 CALL :SUB_WGET ext-tools/utils ottoaccord.tar.gz
+CALL :SUB_WGET ext-tools/utils deployfile.sh
 
 REM
 REM  -- Install cygwin
 REM
-call getcygwin.bat
+CALL getcygwin.bat
 
 REM
 REM  -- install Java
@@ -70,28 +70,28 @@ ECHO "installation complete"
 REM
 REM  -- install gradle
 REM
-echo INSTALL GRADLE...
-copy gradle-2.6.tar "%ACCORD_HOME%"
-copy 7z.tar "%ACCORD_HOME%"
+ECHO INSTALL GRADLE...
+COPY gradle-2.6.tar "%ACCORD_HOME%"
+COPY 7z.tar "%ACCORD_HOME%"
 PUSHD "%ACCORD_HOME%"
-    cd bin
+    CD bin
     c:\cygwin\bin\tar.exe xvf ../7z.tar
-    cd ..
+    CD ..
     c:\cygwin\bin\tar.exe xvf gradle-2.6.tar
-    del gradle-2.6.tar 7z.tar
+    DEL gradle-2.6.tar 7z.tar
 POPD
-echo GRADLE INSTALLATION COMPLETE
-echo Current directory = %CD%
+ECHO GRADLE INSTALLATION COMPLETE
+ECHO Current directory = %CD%
 
 REM
 REM  -- install jenkins
 REM
-echo Uncompressing JENKINS...
-"c:\Program Files\Accord\bin\7z" e jenkins-1.624.zip
-echo Installing JENKINS...
-call c:\Windows\System32\msiexec.exe /i jenkins.msi /qn /l*vx jenkins.log
-echo Completed JENKINS installation!
-echo Please allow a couple of minutes for jenkins to start up
+ECHO Uncompressing JENKINS...
+"c:\Accord\bin\7z" e jenkins-1.624.zip
+ECHO Installing JENKINS...
+CALL c:\Windows\System32\msiexec.exe /i jenkins.msi /qn /l*vx jenkins.log
+ECHO Completed JENKINS installation!
+ECHO Please allow a couple of minutes for jenkins to start up
 
 GOTO :EOF
 
