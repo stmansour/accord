4080dd95da1443b804dd5806a9b5ea6bf653eaab
Steve Mansour <sman@stevemansour.com>
2015-08-28 12:51:12 -0700

    began defining variables in Makefiles. fixes to deployfile.sh

--- devtools/tools/deployfile.sh
+++ devtools/tools/deployfile.sh
@@ -3,7 +3,7 @@
 usage() {
     echo
     echo "Purpose: Deploy a local file to Artifactory keeping the same file name."
-    echo "Usage:   $0 localFilePath targetFolder"
+    echo "Usage:   $0 localFname targetFolder"
     echo "Example: $0 ottoaccord.tar.gz ext-tools/utils"
     echo
     exit 1
@@ -16,38 +16,58 @@ fi
 targetPath="http://ec2-52-6-164-191.compute-1.amazonaws.com/artifactory"
 
 echo "deploying to $targetPath"
-localFilePath="$1"
+localFname="$1"
 targetFolder="$2"
-artifactoryUser="user"
-artifactoryPassword="AP6k6bTdGdXg3Njs"
+artu="user"
+artp="AP6k6bTdGdXg3Njs"
 
-if [ ! -f "$localFilePath" ]; then
-    echo "ERROR: local file $localFilePath does not exists!"
+if [ ! -f "$localFname" ]; then
+    echo "ERROR: local file $localFname does not exists!"
     exit 1
 fi
 
-if [ "$(uname)" = "Darwin" ]; then
-    MD5="md5 -r"
-    md5Value="$($MD5 "$localFilePath")"
-    md5Value="${md5Value:0:32}"
-    echo "MD5 = $md5Value $localFilePath"
-else
-    MD5="md5sum"
-    which $MD5 || exit $?
-    md5Value="$($MD5 "$OPTS" "$localFilePath")"
-    md5Value="${md5Value:0:32}"
-    which sha1sum || exit $?
-    sha1Value="$(sha1sum "$localFilePath")"
-    sha1Value="${sha1Value:0:40}"
-    echo "MD5 and SHA1 = $md5Value $sha1Value $localFilePath"
-fi
+sysname=$(uname)
 
-fileName="$(basename "$localFilePath")"
+case "${sysname}" in
+    "Darwin" )
+	MD5="md5 -r"
+	md5Value="$($MD5 "$localFname")"
+	md5Value="${md5Value:0:32}"
+	echo "MD5 = $md5Value $localFname"
+	;;
 
-echo "INFO: Uploading $localFilePath to $targetPath/targetFolder/$fileName"
-if [ "$(uname)" = "Darwin" ]; then
-    echo "curl -i -X PUT -u $artifactoryUser:$artifactoryPassword -H \"X-Checksum-Md5: $md5Value\" -T \"$localFilePath\" \"$targetPath/$targetFolder/$fileName\""
-    curl -i -X PUT -u $artifactoryUser:$artifactoryPassword -H "X-Checksum-Md5: $md5Value" -T "$localFilePath" "$targetPath/$targetFolder/$fileName"
-else
-    curl -i -X PUT -u $artifactoryUser:$artifactoryPassword -H "X-Checksum-Md5: $md5Value" -H "X-Checksum-Sha1: $sha1Value" -T "$localFilePath"  "$targetPath/$targetFolder/$fileName"
-fi
+    "MINGW32_NT-6.2" | "CYGWIN_NT-6.2" )
+	echo "will attempt to md5sum $localFname"
+	MD5="md5sum"
+	which $MD5 || exit $?
+	md5Value="$($MD5 "$localFname")"
+	md5Value="${md5Value:0:32}"
+	;;
+
+    * )
+	echo "will attempt to md5sum $localFname"
+	MD5="md5sum"
+	which $MD5 || exit $?
+	md5Value="$($MD5 "$localFname")"
+	md5Value="${md5Value:0:32}"
+	which sha1sum || exit $?
+	sha1Value="$(sha1sum "$localFname")"
+	sha1Value="${sha1Value:0:40}"
+	echo "MD5 and SHA1 = $md5Value $sha1Value $localFname"
+	;;
+esac
+
+fname="$(basename "$localFname")"
+
+echo "INFO: Uploading $localFname to $targetPath/$targetFolder/$fname"
+case "${sysname}" in
+    "Darwin" | "MINGW32_NT-6.2" | "CYGWIN_NT_6.2" )
+	echo "curl for ${sysname}"
+	curl -i -X PUT -u $artu:$artp -H "X-Checksum-Md5: $md5Value" -T "$localFname" "$targetPath/$targetFolder/$fname"
+	;;
+
+    * )
+	echo "curl for ${sysname}"
+	curl -i -X PUT -u $artu:$artp -H "X-Checksum-Md5: $md5Value" -H "X-Checksum-Sha1: $sha1Value" -T "$localFname"  "$targetPath/$targetFolder/$fname"
+	;;
+esac
