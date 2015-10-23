d75be6ccb2a4e37bc20bc124958debd2f33f6564
Steve Mansour <sman@stevemansour.com>
2015-10-23 11:17:54 -0700

    aws updated their windows base image ami. Updated scripts account for this

--- cloud/cr_win_jenkins.sh
+++ cloud/cr_win_jenkins.sh
@@ -6,6 +6,7 @@
 MAXINSTANCES=20
 instances=1
 ask=1
+AMI="ami-c9cea0ac"
 if [ $# -gt 0 ]; then 
     while [ $ask -gt 0 ]; do
 	echo -n "Create $1 instances, are you sure? [y/n]: "
@@ -30,6 +31,6 @@ if [ $# -gt 0 ]; then
 fi
 pushd /usr/local/accord/bin
 echo "Creating ${instances} instances..."
-aws ec2 run-instances --output json --image-id ami-417bcf2a --count ${instances} --instance-type t2.micro --key-name smanAWS1  --security-groups Windows-Jenkins --user-data file://winjenk.scr > new-windows-jenkins-instance.json
+aws ec2 run-instances --output json --image-id ${AMI} --count ${instances} --instance-type t2.micro --key-name smanAWS1  --security-groups Windows-Jenkins --user-data file://winjenk.scr > new-windows-jenkins-instance.json
 popd
 date
