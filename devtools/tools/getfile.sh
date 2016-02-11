c6f4c793d2320c8f3f5bc800d12a07ce8abf3d6f
Steve Mansour <sman@stevemansour.com>
2016-02-11 13:47:27 -0800

    updated to support chmod after download

--- devtools/tools/getfile.sh
+++ devtools/tools/getfile.sh
@@ -1,27 +1,76 @@
 #!/bin/bash
+VERBOSE=0
+WGETOPTS="-q"
 
 usage() {
-	echo
-    echo "Purpose: Download a file from artifactory to the local system"
-    echo "Usage:   $0 remotefilepath"
-    echo "Example: $0  ext-tools/utils/s.bat"
-    echo
-    exit 1
+    cat <<ZZEOF
+Purpose: Download a file from artifactory to the local system
+Usage:   $0 [options] remotefilepath
+
+options:
+-m <chown permissions>    # set permissions after download
+-v                        # verbose mode, helps if debugging an issue
+
+Examples:
+
+$0  ext-tools/utils/s.bat
+	This command will download s.bat from Artifactory directory ext-tools/utils
+
+$0	-m 400 db/config.json
+	Download config.json, set its permissions to 400 (only owner can read)
+
+ZZEOF
 }
 
-if [ -z "$1" ]; then
-    usage
-fi
+while getopts ":m:v" o; do
+    case "${o}" in
+    	m)
+            MODE=${OPTARG}
+            ;;
+        v)
+			VERBOSE=1
+			;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 
-APATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
-ARTIF=${APATH}/artifactory.url
-ARTPSW=${APATH}/credp
-targetPath=$(cat ${ARTIF})
-artp=$(cat ${ARTPSW})
-artu="accord"
+for arg do
+	remoteFile=${arg}
+	rfbase=$(basename ${remoteFile})
+	if [ -e ${rfbase} ]; then
+		mv -f ${rfbase} ${rfbase}.save
+	fi
+	APATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+	ARTIF=${APATH}/artifactory.url
+	ARTPSW=${APATH}/credp
+	targetPath=$(cat ${ARTIF})
+	artp=$(cat ${ARTPSW})
+	artu="accord"
 
-echo "reading from ${targetPath}"
-remoteFile="$1"
+	if [ ${VERBOSE} -gt 0 ]; then
+		echo "reading from ${targetPath}"
+		echo "wget --user=${artu} --password=${artp} ${targetPath}/${remoteFile}"
+		WGETOPTS=
+	fi
+	wget ${WGETOPTS} --user=${artu} --password=$artp ${targetPath}/"${remoteFile}" >/tmp/getfile.out
 
-echo "wget --user=${artu} --password=${artp} ${targetPath}/${remoteFile}"
-wget --user=${artu} --password=$artp ${targetPath}/"${remoteFile}"
+	if [ -e ${rfbase} ]; then
+		if [ "${MODE}" != "" ]; then
+			chmod ${MODE} ${rfbase}
+		fi
+		rm ${rfbase}.save
+	else
+		echo "failed to download requested file"
+		if [ -e ${rfbase}.save ]; then
+			mv ${rfbase}.save ${rfbase}
+		fi
+	fi
+	exit 0
+done
+
+usage
+exit 1
