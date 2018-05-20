dee0f141adc419d7050d6029906558a20427eb79
Steve Mansour <sman@stevemansour.com>
2018-05-20 15:06:44 -0700

    FIX: improvements to cr_linux_jenkins script

--- cloud/cr_linux_jenkins.sh
+++ cloud/cr_linux_jenkins.sh
@@ -1,27 +1,62 @@
 #!/bin/bash
 #  This script creates a new running instance of the Linux Jenkins build server.
-#  The server is configured with the last known backup of the running Linux Jenkins server.
-#  The jobs from the last backup of the Linux Jenkins server are also restored.
 
 AMI="ami-14c5486b"
 INSTANCESIZE="t2.micro"
 #INSTANCESIZE="t2.medium"
 KEYNAME="smanAWS1"
 
-MAXINSTANCES=20
-instances=1
-ask=1
-if [ $# -gt 0 ]; then 
+MAXINSTANCES=5
+INSTANCECOUNT=1
+
+usage() {
+	cat <<EOF
+
+SYNOPSIS
+	$0 [-a -m -n]
+
+	Create a new Jenkins instance in the AWS cloud. When run with no options,
+	this script creates a new t2.micro EC2 instance and loads the code needed
+	to compile and test the Roller suite. The server is configured with the
+	last known backup of the running Linux Jenkins server, this includes the
+	users, the plugins, and the jobs.
+
+OPTIONS
+    -a amiName
+       Create instances using the system image specified by amiName. The
+       default image is ami-14c5486b.
+
+    -m instanceModel
+      Option m is used to define which instance model you want to create.
+      The default module is t2.micro.
+
+    -n numberOfInstances
+      Option n is used to set the number of INSTANCECOUNT to be created. The
+      default is to create a single instance.
+
+EXAMPLES:
+    This will create a single instance of a t2.micro machine with Jenkins
+
+        $ cr_linux_jenkins.sh
+
+    This will create a single instance of a t2.medium machine with Jenkins
+
+        $ cr_linux_jenkins.sh -m t2.medium
+
+EOF
+}
+
+askYesNo() {
+	ask=1
     while [ $ask -gt 0 ]; do
-	echo -n "Create $1 instances, are you sure? [y/n]: "
+	echo -n "Create ${INSTANCECOUNT} instances, are you sure? [y/n]: "
 	read ans
 	case $ans in
 	    [yY] | [yY][Ee][Ss] )
-		instances=$1
 		ask=0
-		if [ $instances -gt $MAXINSTANCES ]; then
-		    echo "My max limit is $MAXINSTANCES. I'll set it to $MAXINSTANCES instances."
-		    instances=$MAXINSTANCES
+		if [ ${INSTANCECOUNT} -gt ${MAXINSTANCES} ]; then
+		    echo "My max limit is ${MAXINSTANCES}. I'll set it to ${MAXINSTANCES} INSTANCECOUNT."
+		    INSTANCECOUNT=${MAXINSTANCES}
 		fi
 		;;
 	    [nN] | [n|N][O|o] )
@@ -32,9 +67,33 @@ if [ $# -gt 0 ]; then
 		;;
 	esac
     done
-fi
+}
+
+while getopts "m:n:" o; do
+	case "${o}" in
+    a)  AMI=${OPTARG}
+        echo "The AMI used for the instances is set to: ${AMI}"
+        ;;
+    m)	INSTANCESIZE=${OPTARG}
+    	echo "INSTANCESIZE has been set to ${INSTANCESIZE}"
+    	;;
+    n) INSTANCECOUNT=${OPTARG}
+		if [ ${INSTANCECOUNT} -gt ${MAXINSTANCES} ]; then
+		    echo "My max limit is ${MAXINSTANCES}."
+		    echo "Exiting without creating any instances"
+		    exit 1
+		fi
+        ;;
+    *) 	usage
+    	exit 1
+    	;;
+	esac
+done
+shift $((OPTIND-1))
+
+
 pushd /usr/local/accord/bin
-echo "Creating ${instances} instances..."
-aws ec2 run-instances --output json --image-id ${AMI} --count ${instances} --instance-type ${INSTANCESIZE} --key-name ${KEYNAME}  --security-groups Linux-Jenkins-CI  --user-data file://setup-linux-jenkins.sh > new-linux-jenkins-instance.json
+echo "Creating ${INSTANCECOUNT} Jenkins instances..."
+aws ec2 run-INSTANCECOUNT --output json --image-id ${AMI} --count ${INSTANCECOUNT} --instance-type ${INSTANCESIZE} --key-name ${KEYNAME}  --security-groups Linux-Jenkins-CI  --user-data file://setup-linux-jenkins.sh > new-linux-jenkins-instance.json
 popd
 date
